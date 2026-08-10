194f837d846008e537aa98ecff2e274ab4ceaed5
harleyiscute507-arch <harleyiscute507@gmail.com>
2026-08-10 20:39:44 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "150+ Commands", // required by Discord API, not shown in the client
         state: "TWG Gorilla Tag Comp Team",     // this is what people actually see
         type: 3,               // Custom
       },
